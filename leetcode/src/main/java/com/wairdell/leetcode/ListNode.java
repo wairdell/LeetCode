66d27c8319f6a31cd475bac780a02b349f20602e
fengqiao <zhouwenchao9711@dingtalk.com>
2023-02-09 13:51:10 +0800

    LeetCode

---
+++ leetcode/src/main/java/com/wairdell/leetcode/ListNode.java
@@ -0,0 +1,16 @@
+package com.wairdell.leetcode;
+
+/**
+ * author : fengqiao
+ * date   : 2023/2/9 13:48
+ * desc   :
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
